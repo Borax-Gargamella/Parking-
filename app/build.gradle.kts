64ef654955d57af9608c21efda57b7e5f2af084e
Borax-Gargamella <157167477+Borax-Gargamella@users.noreply.github.com>
2025-02-06 23:06:42 +0100

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,22 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.1.1"))
+
+    // Firebase Auth
+    implementation("com.google.firebase:firebase-auth")
+
+    // Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore")
+
+    // ZXing per QR code
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
+    // Glide (se ti serve per caricare immagini) Per gestire immagini (se vuoi usare Glide o simili, anche se qui hai immagini locali)
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
 }
+
+
